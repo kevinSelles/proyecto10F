c5186b423a2afc0955d85bed50e1a24e46d6e999
kevinSelles <kevin.selles.c@gmail.com>
2025-09-24 02:17:51 +0200

    Update API.js

--- eventFront/src/utils/API.js
+++ eventFront/src/utils/API.js
@@ -1,4 +1,4 @@
-const url = import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
+const url = "https://splish-splash-back.vercel.app/api/";
 
 export const API = async ({ endpoint, method = "GET", body, isJSON = true }) => {
   try {
